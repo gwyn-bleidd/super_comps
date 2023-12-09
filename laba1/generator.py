3951f393800327b39ca8037a2570d2ba2e937004
gwyn-bleidd <yndx-gwyn@yandex.ru>
2023-12-10 00:15:09 +0300

    f

--- laba1/generator.py
+++ laba1/generator.py
@@ -5,4 +5,4 @@ n = int(sys.argv[1])
 with open("test.txt", 'w') as out:
     out.write(str(n) + "\n")
     for i in range(n+1):
-        out.write(" ".join(map(str, [random.randint(0,22) for x in range(n)])) + "\n")
+        out.write(" ".join(map(str, [random.uniform(-10, 10) for x in range(n)])) + "\n")
